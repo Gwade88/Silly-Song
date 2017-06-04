344801d4422d105117930d9ca2cf47e230f47e49
Garret Thomas <Garret.Wade.Thomas@Gmail.com>
2017-06-04 08:39:56 -0500

    Modified text field

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var lyricsView: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+        nameField.text?.capitalized
+    }
+    @IBAction func displayLyrics(_ sender: Any) {
+        if nameField.text != "" {
+            lyricsView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+        }
+    }
 
 }
 
+func shortNameFromName(_ name: String) -> String {
+    var lowercaseName = name.lowercased()
+    lowercaseName = lowercaseName.folding(options: .diacriticInsensitive, locale: nil)
+    let vowelSet = CharacterSet(charactersIn: "AEIOUaeiou")
+    
+    if let firstVowelRange = name.rangeOfCharacter(from: vowelSet, options: .caseInsensitive) {
+        return lowercaseName.substring(from: firstVowelRange.lowerBound)
+    }
+    
+    // no vowels = return the full name
+    return lowercaseName
+}
+
+
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    
+    
+    let shortName = shortNameFromName(fullName)
+    
+    let lyrics = lyricsTemplate
+        .replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    
+    return lyrics
+    
+}
+
+//lyricsForName(lyricsTemplate: String, fullName: "Juliette")
+
+// join an array of strings into a single template string:
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
